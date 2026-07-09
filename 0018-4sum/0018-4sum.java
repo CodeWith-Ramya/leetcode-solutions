aa7ae38d54d9ab2b3e753e8a4904261c3f3e7180
S Ramya <ramya15315716@gmail.com>
2026-07-09 16:58:40 +0530

    Time: 396 ms (5.03%), Space: 45.6 MB (77.31%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -6,31 +6,24 @@ class Solution {
         for(int i =0;i<n-3;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j =i+1;j<n-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                int l =j+1;
-                int r =  n-1;
-                while(l<r){
-                    long sum = (long)nums[i]+nums[j]+nums[l]+nums[r];
-                    if(sum == target){
-                        res.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
-                        l++;
-                        r--;
-
-                        while(l<r && nums[l]==nums[l-1]) l++;
-                        while(l<r && nums[r]==nums[r+1]) r--;
-                        
-
-                    }
-                    else if(sum>target){
-                        r--;
-                    }
-                    else {
-                        l++;
+                 if(j>i+1 && nums[j]==nums[j-1]) continue;
+                for(int k =j+1;k<n-1;k++){
+                    if(k>j+1 && nums[k]==nums[k-1]) continue;
+                    for(int l=k+1;l<n;l++){
+                        if(l > k+1 && nums[l] == nums[l-1]) continue;
+                        long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
+                        if(sum== target){
+                            res.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        }
                     }
                 }
-
             }
-        }
+
+        }   
+        
+        
+        
         return res;
-    }
+        
+         }
 }
